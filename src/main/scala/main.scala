349d161c21b5090bcbdb80d4a0c198141ffdb09d
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2024-06-14 13:42:59 +0200

    Remove Unit from main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,7 @@
-object main {
+object main:
 
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-  }
+  def main(args: Array[String]) =
+    val card = new cards.SimpleCardImpl(5, utils.Color.Red)
+    println(card)
 
-}
+
