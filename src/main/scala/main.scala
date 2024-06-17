c8072d64c9c282723c7bbf23b02c93a8cd83254e
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2024-06-17 17:37:17 +0200

    Fix main to not have any error left

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,10 @@
+import utils.Color.Red
+import utils.ImageHandler.loadCardImage
+
 object main:
 
   def main(args: Array[String]) =
-    val card = new cards.SimpleCardImpl(5, utils.Color.Red)
+    val card = new cards.SimpleCardImpl(5, utils.Color.Red, loadCardImage("5", Red))
     println(card)
 
 
