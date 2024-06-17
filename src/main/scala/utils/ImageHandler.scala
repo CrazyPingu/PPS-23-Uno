2555f514f0bfa372a55226291269507f84b8cccb
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2024-06-17 17:34:44 +0200

    Create a method to handle the reading of images

---
+++ src/main/scala/utils/ImageHandler.scala
@@ -0,0 +1,23 @@
+package utils
+
+import java.awt.Image
+import java.io.{File, IOException}
+import javax.imageio.ImageIO
+
+object ImageHandler:
+
+  private def loadImage(path: String): Image =
+    try {
+      ImageIO.read(new File("src/main/resources/" + path))
+    } catch {
+      case e: IOException =>
+        System.err.println(s"Error loading image: $path")
+        e.printStackTrace()
+        null
+    }
+
+
+  def loadCardImage(cardName: String, color: Color): Image =
+    loadImage("cards/" + color.toString + "/" + color.toString + "_" + cardName + ".png")
+
+
