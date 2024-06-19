8b9609e90359fb17b57aeb483f5ff79bd74e6722
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2024-06-19 11:56:49 +0200

    Removed junit library and fixed return type of main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,8 +3,11 @@ import utils.ImageHandler.loadCardImage
 
 object main:
 
-  def main(args: Array[String]) =
+  def main(args: Array[String]): Unit =
     val card = new cards.SimpleCardImpl(5, utils.Color.Red, loadCardImage("5", Red))
     println(card)
+
+    for _ <- 0 to 1 do
+      println("Hello, world!")
 
 
