0643801d83ea8b220e3eb56be01b7c7619d1b46d
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2024-07-09 02:22:48 +0200

    Add basic JFrame

---
+++ src/main/scala/view/Frame.scala
@@ -0,0 +1,10 @@
+package view
+
+import javax.swing.{JFrame, WindowConstants}
+
+class Frame extends JFrame:
+  setTitle("UNO")
+  setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+  setSize(1280, 720)
+  setResizable(false)
+
