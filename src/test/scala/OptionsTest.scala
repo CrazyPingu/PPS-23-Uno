af15de121a14d0273baee625f207b3d6af02140e
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-11 01:07:20 +0200

    Create OptionsTest.scala

---
+++ src/test/scala/OptionsTest.scala
@@ -0,0 +1,12 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class OptionsTest extends AnyFunSuite:
+
+  test("Updating the settings"):
+    assert(true)
+
+  test("Saving the settings"):
+    assert(true)
+
+  test("Loading the settings"):
+    assert(true)
