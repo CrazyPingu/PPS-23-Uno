ad25caadc0f69f788b2f589f6fc359297c50a43f
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-10 23:42:43 +0200

    Update AchievementTest.scala

--- src/test/scala/AchievementTest.scala
+++ src/test/scala/AchievementTest.scala
@@ -1,8 +1,22 @@
+import model.achievements.PropertyImpl
 import org.scalatest.funsuite.AnyFunSuite
+import utils.ComparisonOperator
 
 class AchievementTest extends AnyFunSuite:
     
-  test("Get achievement"):
+  test("Achieve something with 1 property"):
+    val property1 = PropertyImpl("test", 1, ComparisonOperator.Equal)
+
+    assert(true)
+
+  test("Achieve something with multiple properties"):
+    assert(true)
+
+  test("Fail to achieve"):
     assert(true)
   
+  test("Save achievement"):
+    assert(true)
 
+  test("Load achievement"):
+    assert(true)
