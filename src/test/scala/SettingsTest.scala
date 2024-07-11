831660d921989f5bb9a4276a58bae0591f5be6cd
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-11 10:14:05 +0200

    Update SettingsTest.scala

--- src/test/scala/SettingsTest.scala
+++ src/test/scala/SettingsTest.scala
@@ -1,12 +1,22 @@
+import model.settings.{GameSettings, Settings, SettingsImpl}
 import org.scalatest.funsuite.AnyFunSuite
 
 class SettingsTest extends AnyFunSuite:
 
   test("Updating the settings"):
-    assert(true)
+    val settings: Settings = SettingsImpl("/jsonTest/settings.json")
+    assert(settings.getSettings.equals(GameSettings.defaultSettings))
+    settings.updateSettings(GameSettings("hard"))
+    assert(settings.getSettings.equals(GameSettings("hard")))
 
-  test("Saving the settings"):
-    assert(true)
+  test("Check if the settings saves and loads the data"):
+    var settings: Settings = SettingsImpl("/jsonTest/settings.json")
+    settings.updateSettings(GameSettings("hard"))
+    settings = SettingsImpl("/jsonTest/settings.json")
+    assert(settings.getSettings.equals(GameSettings("hard")))
 
-  test("Loading the settings"):
-    assert(true)
+  test("Resetting the settings"):
+    val settings: Settings = SettingsImpl("/jsonTest/settings.json")
+    assert(settings.getSettings.equals(GameSettings("hard")))
+    settings.resetSettings()
+    assert(settings.getSettings.equals(GameSettings.defaultSettings))
