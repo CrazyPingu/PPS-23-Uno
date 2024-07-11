d698980815bfbd3ee087086ecd73efc19a507a51
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-11 10:42:00 +0200

    Update path generation

--- src/test/scala/SettingsTest.scala
+++ src/test/scala/SettingsTest.scala
@@ -1,22 +1,33 @@
 import model.settings.{GameSettings, Settings, SettingsImpl}
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 
-class SettingsTest extends AnyFunSuite:
+import java.nio.file.{Files, Paths}
+
+class SettingsTest extends AnyFunSuite with BeforeAndAfterAll:
+
+  val projectRoot: String = System.getProperty("user.dir")
+  val settingsFilePath: String = s"$projectRoot/settingsTest/settings.json"
 
   test("Updating the settings"):
-    val settings: Settings = SettingsImpl("/jsonTest/settings.json")
+    val settings: Settings = SettingsImpl(settingsFilePath)
     assert(settings.getSettings.equals(GameSettings.defaultSettings))
     settings.updateSettings(GameSettings("hard"))
     assert(settings.getSettings.equals(GameSettings("hard")))
 
   test("Check if the settings saves and loads the data"):
-    var settings: Settings = SettingsImpl("/jsonTest/settings.json")
+    var settings: Settings = SettingsImpl(settingsFilePath)
     settings.updateSettings(GameSettings("hard"))
-    settings = SettingsImpl("/jsonTest/settings.json")
+    settings = SettingsImpl(settingsFilePath)
     assert(settings.getSettings.equals(GameSettings("hard")))
 
   test("Resetting the settings"):
-    val settings: Settings = SettingsImpl("/jsonTest/settings.json")
+
+    val settings: Settings = SettingsImpl(settingsFilePath)
     assert(settings.getSettings.equals(GameSettings("hard")))
     settings.resetSettings()
     assert(settings.getSettings.equals(GameSettings.defaultSettings))
+
+  override def afterAll(): Unit =
+    Files.delete(Paths.get(settingsFilePath))
+    Files.delete(Paths.get(settingsFilePath).getParent)
