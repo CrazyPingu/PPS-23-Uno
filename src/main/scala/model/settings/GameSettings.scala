0781bc7969553f8635598489d9ca575bca8e3c44
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-11 09:51:36 +0200

    Update GameSettings.scala

--- src/main/scala/model/settings/GameSettings.scala
+++ src/main/scala/model/settings/GameSettings.scala
@@ -1,3 +1,8 @@
 package model.settings
 
+import play.api.libs.json.{Json, OFormat}
+
 case class GameSettings(difficulty: String)
+
+object GameSettings:
+  implicit val gameSettingsFormat: OFormat[GameSettings] = Json.format[GameSettings]
