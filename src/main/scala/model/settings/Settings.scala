60deadbd8a0a551e4d03fcbd52cf2f62ba1ab67f
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-11 09:51:31 +0200

    Update Settings.scala

--- src/main/scala/model/settings/Settings.scala
+++ src/main/scala/model/settings/Settings.scala
@@ -2,10 +2,8 @@ package model.settings
 
 trait Settings:
 
-  def loadSettings(): Option[GameSettings]
-
-  def saveSettings(settings: GameSettings): Unit
-
   def getSettings: GameSettings
 
   def updateSettings(newSettings: GameSettings): Unit
+
+  def resetSettings(): Unit
