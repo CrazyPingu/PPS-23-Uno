236bccceb45116b5ad01b8529f927247ea098251
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-11 09:51:21 +0200

    Create JsonUtils.scala

---
+++ src/main/scala/utils/JsonUtils.scala
@@ -0,0 +1,19 @@
+package utils
+
+import play.api.libs.json.{Json, Reads, Writes}
+
+import java.nio.file.{Files, Paths, StandardOpenOption}
+
+object JsonUtils:
+
+  def loadFromFile[T](filePath: String)(implicit reads: Reads[T]): Option[T] =
+    val path = Paths.get(filePath)
+    if Files.exists(path) then
+      val json = new String(Files.readAllBytes(path))
+      Json.parse(json).asOpt[T]
+    else
+      None
+
+  def saveToFile[T](filePath: String, data: T)(implicit writes: Writes[T]): Unit =
+    val json = Json.prettyPrint(Json.toJson(data))
+    Files.write(Paths.get(filePath), json.getBytes, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
