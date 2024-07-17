853b98ac0cf968e99329c16b8aedc7fe7f8d7d13
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-18 00:35:39 +0200

    Add settingsGui in main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,9 @@
 import controller.{Controller, GameLoop}
 import model.bot.EasyBotPlayerImpl
 import model.{Deck, Hand}
+import view.CardLayoutId.Settings
 import view.game.Gui
+import view.settings.SettingsGui
 import view.{CardLayoutId, Frame}
 
 object main:
@@ -11,8 +13,14 @@ object main:
     val controller = new Controller()
     val gui = new Gui(controller)
     controller.setGui(gui)
+
+    val settingsGui = new SettingsGui()
+
     val frame = new Frame()
     frame.add(gui, CardLayoutId.Game)
+    frame.add(settingsGui, CardLayoutId.Settings)
+
     frame.show(CardLayoutId.Game)
+    //frame.show(CardLayoutId.Settings)
 
     new GameLoop(controller, gui).start()
