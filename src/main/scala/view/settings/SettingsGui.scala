7d12eb695a185d47b0b423dd5f5f594d4d00d602
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-21 14:34:51 +0200

    Update SettingsGui.scala

--- src/main/scala/view/settings/SettingsGui.scala
+++ src/main/scala/view/settings/SettingsGui.scala
@@ -3,8 +3,9 @@ package view.settings
 import model.settings.{GameSettings, Settings, SettingsImpl}
 import view.game.CoordinateHandler.panelGridDimension
 
-import java.awt.GridLayout
-import javax.swing.{JButton, JPanel}
+import java.awt.event.{ActionEvent, ActionListener}
+import java.awt.{Component, Dimension, FlowLayout, GridLayout}
+import javax.swing.{Box, JButton, JPanel}
 
 class SettingsGui extends JPanel:
   private val layout: GridLayout = new GridLayout(panelGridDimension(0), panelGridDimension(1))
@@ -15,13 +16,18 @@ class SettingsGui extends JPanel:
 
   private val settings : Settings = SettingsImpl(settingsFilePath)
 
-  private val button1: JButton = new JButton("Button 1")
+  private val difficultyButton: JButton = new JButton("Difficulty")
   private val button2: JButton = new JButton("Button 2")
+  private val goBackButton: JButton = new JButton("Go Back to Menu")
 
-  add(button1)
-  add(button2)
+  private val buttonDimension = new Dimension(200, 100)
+  difficultyButton.setPreferredSize(buttonDimension)
+  button2.setPreferredSize(buttonDimension)
+  goBackButton.setPreferredSize(buttonDimension)
+  
+  goBackButton.addActionListener((e: ActionEvent) => println("Go Back to Menu button pressed"))
 
-
-
-
-
+  private def createCenteredPanel(componentList: List[Component]): JPanel =
+    val panel = new JPanel(new FlowLayout(FlowLayout.CENTER))
+    componentList.foreach(panel.add)
+    panel
