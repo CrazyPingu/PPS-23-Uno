c6b026578eadd23f9ecada126e78093130bae32f
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-22 00:21:18 +0200

    Add jpanels and save button

--- src/main/scala/view/settings/SettingsGui.scala
+++ src/main/scala/view/settings/SettingsGui.scala
@@ -4,8 +4,8 @@ import model.settings.{GameSettings, Settings, SettingsImpl}
 import view.game.CoordinateHandler.panelGridDimension
 
 import java.awt.event.{ActionEvent, ActionListener}
-import java.awt.{Component, Dimension, FlowLayout, GridLayout}
-import javax.swing.{Box, JButton, JPanel}
+import java.awt.GridLayout
+import javax.swing.{Box, JButton, JComboBox, JLabel, JPanel}
 
 class SettingsGui extends JPanel:
   private val layout: GridLayout = new GridLayout(panelGridDimension(0), panelGridDimension(1))
@@ -13,21 +13,45 @@ class SettingsGui extends JPanel:
 
   private val projectRoot: String = System.getProperty("user.dir")
   private val settingsFilePath: String = s"$projectRoot/config/settings.json"
-
   private val settings : Settings = SettingsImpl(settingsFilePath)
 
-  private val difficultyButton: JButton = new JButton("Difficulty")
-  private val button2: JButton = new JButton("Button 2")
+  add(Box.createVerticalGlue())
+
+  private val difficultyPanel : JPanel = new JPanel()
+  private val difficultyLabel : JLabel = new JLabel("Difficulty")
+  private val difficultyOptions: JComboBox[String] = new JComboBox(Array("Easy", "Medium", "Hard"))
+  difficultyPanel.add(difficultyLabel)
+  difficultyPanel.add(difficultyOptions)
+  add(difficultyPanel)
+
+  add(Box.createVerticalStrut(10))
+
+  private val button2Panel : JPanel = new JPanel()
+  private val button2Label : JLabel = new JLabel("Button 2")
+  private val button2Options: JComboBox[String] = new JComboBox(Array("Option 1", "Option 2", "Option 3"))
+  button2Panel.add(button2Label)
+  button2Panel.add(button2Options)
+  add(button2Panel)
+
+  add(Box.createVerticalStrut(20))
+
+  private val endPanel : JPanel = new JPanel()
+  private val saveSettings : JButton = new JButton("Save settings")
   private val goBackButton: JButton = new JButton("Go Back to Menu")
+  endPanel.add(saveSettings)
+  endPanel.add(goBackButton)
+  add(endPanel)
 
-  private val buttonDimension = new Dimension(200, 100)
-  difficultyButton.setPreferredSize(buttonDimension)
-  button2.setPreferredSize(buttonDimension)
-  goBackButton.setPreferredSize(buttonDimension)
-  
-  goBackButton.addActionListener((e: ActionEvent) => println("Go Back to Menu button pressed"))
+  add(Box.createVerticalGlue())
 
-  private def createCenteredPanel(componentList: List[Component]): JPanel =
-    val panel = new JPanel(new FlowLayout(FlowLayout.CENTER))
-    componentList.foreach(panel.add)
-    panel
+  saveSettings.addActionListener((e: ActionEvent) => settings.updateSettings(GameSettings(
+    difficultyOptions.getSelectedItem.toString
+    )))
+
+  goBackButton.addActionListener((e: ActionEvent) =>
+    println("Go Back to Menu button pressed"))
+
+  //override protected def paintComponent(g: Graphics): Unit =
+  //  super.paintComponent(g)
+  //  g.setColor(Color.RED)
+  //  g.fillRect(0, 0, getWidth, getHeight)
