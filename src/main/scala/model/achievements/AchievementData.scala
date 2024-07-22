c64d110b1a4d471ab0024f57bf90780265200095
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-22 10:13:02 +0200

    Fix object name

--- src/main/scala/model/achievements/AchievementData.scala
+++ src/main/scala/model/achievements/AchievementData.scala
@@ -4,5 +4,5 @@ import play.api.libs.json.{Json, OFormat}
 
 case class AchievementData(ID: Int, description: String, isAchieved: Boolean)
 
-object GameSettings:
+object AchievementData:
   implicit val gameSettingsFormat: OFormat[AchievementData] = Json.format[AchievementData]
