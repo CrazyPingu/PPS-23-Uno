d16f7965f40162a581b8467eb0b4560da2887080
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-22 12:45:43 +0200

    Add Difficulty with Enumeration

--- src/main/scala/model/settings/GameSettings.scala
+++ src/main/scala/model/settings/GameSettings.scala
@@ -1,9 +1,25 @@
 package model.settings
 
-import play.api.libs.json.{Json, OFormat}
+import model.settings.Difficulty.{Difficulty, Easy}
+import play.api.libs.json.{Format, Json, OFormat, Reads, Writes}
 
-case class GameSettings(difficulty: String)
+case class GameSettings(difficulty: Difficulty, startCardValue: Int)
 
 object GameSettings:
   implicit val gameSettingsFormat: OFormat[GameSettings] = Json.format[GameSettings]
-  val defaultSettings: GameSettings = GameSettings("Easy")
+  val defaultSettings: GameSettings = GameSettings(Difficulty.Easy, 7)
+
+object Difficulty extends Enumeration:
+  type Difficulty = Value
+  val Easy, Hard = Value
+  implicit val difficultyFormat: Format[Difficulty] = Json.formatEnum(Difficulty)
+
+  def fromInt(value: Int): Difficulty = value match
+    case 0 => Easy
+    case 1 => Hard
+    case _ => throw new IllegalArgumentException("Invalid difficulty value")
+
+  def toInt(difficulty: Difficulty): Int = difficulty match
+    case Easy => 0
+    case Hard => 1
+
