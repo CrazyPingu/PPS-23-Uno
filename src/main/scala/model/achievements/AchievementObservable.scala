e574342bedbb5508f030a0ee37de57e5dba13386
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-23 22:49:25 +0200

    Delete trait

--- src/main/scala/model/achievements/AchievementObservable.scala
+++ src/main/scala/model/achievements/AchievementObservable.scala
@@ -1,7 +1,17 @@
-package model.achievements
+package model.achievements;
 
-trait AchievementObservable extends Observable:
+class AchievementObservable(private val filePath: String) extends Observable:
+  private var achievementList: List[Achievement] = List()
 
-  def saveAchievements(): Unit
+  override def addObserver(observer: Observer): Unit = observer match
+    case observer: Achievement => achievementList = observer :: achievementList
+    case _ => throw new IllegalArgumentException("Invalid observer type. It must be an Achievement.")
+
+  override def addObservers(observers: List[Observer]): Unit = 
+    observers.foreach(addObserver)
   
-  def loadAchievements(): Unit
+  override def removeObserver(observer: Observer) : Unit =
+    achievementList = achievementList.filterNot(_ == observer)
+  
+  override def notifyObserver(event: Event) : Unit =
+    achievementList.foreach(_.update(event))
