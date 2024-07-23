a1b7de4c91c0f8393dbbbaf0a2dea4eb02714055
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-23 22:28:03 +0200

    Update AchievementGui.scala

--- src/main/scala/view/achievements/AchievementGui.scala
+++ src/main/scala/view/achievements/AchievementGui.scala
@@ -2,6 +2,7 @@ package view.achievements
 
 import controller.PageController
 
+import java.awt.GridBagLayout
 import javax.swing.JPanel
 
 class AchievementGui(pageController: PageController) extends JPanel:
