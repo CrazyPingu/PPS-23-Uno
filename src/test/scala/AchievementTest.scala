e9b917e067b5e0d203bf0fa2e6e404e8a8007e37
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-23 17:07:08 +0200

    Updated test with new achievement class

--- src/test/scala/AchievementTest.scala
+++ src/test/scala/AchievementTest.scala
@@ -1,34 +1,36 @@
-import model.achievements.AchievementImpl
+import model.achievements.{Achievement, Event}
 import org.scalatest.funsuite.AnyFunSuite
-import utils.{ComparisonOperator, Event}
+import utils.ComparisonOperator
 
 class AchievementTest extends AnyFunSuite:
 
-  test("Should get the achievement name."):
-    val property = AchievementImpl(0, "test", 1, ComparisonOperator.Equal)
-    assert(property.name == "test")
+  test("Should get the achievement data."):
+    val achievement = Achievement(0, "test", false, 1, ComparisonOperator.Equal)
+    assert(achievement.id.equals(0))
+    assert(achievement.description.equals("test"))
+    assert(achievement.isAchieved.equals(false))
 
   test("Should trigger when called by Event."):
-    val achievement = AchievementImpl(0, "test", 1, ComparisonOperator.Equal)
-    achievement.update(Event("test", 0))
-    assert(!achievement.checkAchievement())
-    achievement.update(Event("not", 1))
-    assert(!achievement.checkAchievement())
-    achievement.update(Event("test", 1))
-    assert(achievement.checkAchievement())
+    val achievement = Achievement(0, "test", false, 1, ComparisonOperator.Equal)
+    achievement.update(Event(1, 1))
+    assert(!achievement.isAchieved)
+    achievement.update(Event(0, 2))
+    assert(!achievement.isAchieved)
+    achievement.update(Event(0, 1))
+    assert(achievement.isAchieved)
 
   test("Should work with different comparison operators."):
-    val achGreater = AchievementImpl(0, "test", 1, ComparisonOperator.Greater)
-    achGreater.update(Event("test", 0))
-    assert(!achGreater.checkAchievement())
-    achGreater.update(Event("test", 2))
-    assert(achGreater.checkAchievement())
+    val achGreater = Achievement(0, "test", false, 1, ComparisonOperator.Greater)
+    achGreater.update(Event(0, 1))
+    assert(!achGreater.isAchieved)
+    achGreater.update(Event(0, 2))
+    assert(achGreater.isAchieved)
 
-    val achLess = AchievementImpl(0, "test", 1, ComparisonOperator.Less)
-    achLess.update(Event("test", 2))
-    assert(!achLess.checkAchievement())
-    achLess.update(Event("test", 0))
-    assert(achLess.checkAchievement())
+    val achLess = Achievement(0, "test", false, 1, ComparisonOperator.Less)
+    achLess.update(Event(0, 1))
+    assert(!achLess.isAchieved)
+    achLess.update(Event(0, 0))
+    assert(achLess.isAchieved)
 
   test("Should save the achievement and load when already exist"):
     assert(true)
