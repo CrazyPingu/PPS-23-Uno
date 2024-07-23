cf8ae399af305cbfbb63c6f1e161be18623ca2a9
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-23 16:52:55 +0200

    Add Json formatting for ComparisonOperator

--- src/main/scala/utils/ComparisonOperator.scala
+++ src/main/scala/utils/ComparisonOperator.scala
@@ -1,5 +1,7 @@
 package utils
 
+import play.api.libs.json.{Format, JsError, JsResult, JsString, JsSuccess, JsValue}
+
 enum ComparisonOperator:
   case Greater
   case Less
@@ -9,3 +11,13 @@ enum ComparisonOperator:
     case Greater => x > y
     case Less => x < y
     case Equal => x == y
+
+implicit val comparisonOperatorFormat: Format[ComparisonOperator] = new Format[ComparisonOperator]:
+  override def reads(json: JsValue): JsResult[ComparisonOperator] = json.as[String] match
+    case "Greater" => JsSuccess(ComparisonOperator.Greater)
+    case "Less" => JsSuccess(ComparisonOperator.Less)
+    case "Equal" => JsSuccess(ComparisonOperator.Equal)
+    case _ => JsError("Unknown comparison operator")
+
+  override def writes(comparator: ComparisonOperator): JsValue = JsString(comparator.toString)
+
