f3b13bc71ac717fa2cef085eab26f26d299c0c34
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-23 20:06:54 +0200

    Fix Settings variables

--- src/main/scala/model/settings/SettingsImpl.scala
+++ src/main/scala/model/settings/SettingsImpl.scala
@@ -4,13 +4,12 @@ import utils.JsonUtils
 
 class SettingsImpl(filePath: String) extends Settings:
 
-  private val path = filePath
   var gameSettings: GameSettings = JsonUtils.loadFromFile[GameSettings](filePath).getOrElse(GameSettings.DEFAULT_SETTINGS)
   
   override def updateSettings(newSettings: GameSettings): Unit =
     gameSettings = newSettings
-    JsonUtils.saveToFile(path, gameSettings)
+    JsonUtils.saveToFile(filePath, gameSettings)
 
   override def resetSettings(): Unit =
     gameSettings = GameSettings.DEFAULT_SETTINGS
-    JsonUtils.saveToFile(path, gameSettings)
+    JsonUtils.saveToFile(filePath, gameSettings)
