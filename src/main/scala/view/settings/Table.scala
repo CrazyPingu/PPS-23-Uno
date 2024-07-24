735d38a34b7da506059b3ef743acfcbaf59577a8
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-07-24 20:18:16 +0200

    Add JTable wrapper

--- src/main/scala/view/settings/Table.scala
+++ src/main/scala/view/settings/Table.scala
@@ -1,8 +1,9 @@
 package view.settings
 
-import java.awt.Color
+import view.Label
+
+import java.awt.{Color, Dimension}
 import javax.swing.{JTable, SwingConstants}
-import javax.swing.border.EmptyBorder
 import javax.swing.table.{DefaultTableCellRenderer, DefaultTableModel, TableColumn}
 
 class Table(data: Array[Array[AnyRef]], columnNames: Array[AnyRef], columnSize: Array[Int]) extends JTable(new DefaultTableModel(data, columnNames)):
@@ -14,13 +15,15 @@ class Table(data: Array[Array[AnyRef]], columnNames: Array[AnyRef], columnSize: 
   setFocusable(false)
   setEnabled(false)
   setFont(getFont.deriveFont(20f))
+  setIntercellSpacing(new Dimension(7, 7))
+  setRowHeight(getRowHeight() + 20)
   applyPadding()
 
   private def applyPadding(): Unit =
     val renderer = new DefaultTableCellRenderer:
 
       override def getTableCellRendererComponent(table: JTable, value: Any, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int): java.awt.Component =
-        val component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
+        val component = new Label(value.toString)
         value match
           case "Achieved" =>
             component.setForeground(Color.GREEN)
@@ -29,10 +32,8 @@ class Table(data: Array[Array[AnyRef]], columnNames: Array[AnyRef], columnSize: 
           case _ =>
             component.setBackground(Color.WHITE)
             component.setForeground(Color.BLACK)
+        component.setHorizontalAlignment(SwingConstants.CENTER)
         component
-
-      setHorizontalAlignment(SwingConstants.CENTER)
-      setBorder(new EmptyBorder(5, 5, 5, 5))
 
     for (i <- 0 until getColumnCount)do
       getColumnModel.getColumn(i).setCellRenderer(renderer)
