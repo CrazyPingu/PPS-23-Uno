112b3c935dd5497f7dc0657386149f2930629a87
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2024-07-30 20:51:33 +0200

    Update GeneralTutorialSlide.scala

--- src/main/scala/view/tutorial/GeneralTutorialSlide.scala
+++ src/main/scala/view/tutorial/GeneralTutorialSlide.scala
@@ -2,7 +2,4 @@ package view.tutorial
 
 import java.awt.Image
 
-class GeneralTutorialSlide(private val guiImage: Image, private val guiTitle : String, private val guiDescription: String):
-  val image: Image = guiImage
-  val title: String = guiTitle
-  val description: String = guiDescription
+class GeneralTutorialSlide(val image: Image, val title : String, val description: String)
