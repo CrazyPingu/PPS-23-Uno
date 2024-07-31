9c28258d048db34f58ff39869478edab8591c1c0
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2024-07-31 17:08:00 +0200

    Fix bots tests error

--- src/test/scala/HardBotTest.scala
+++ src/test/scala/HardBotTest.scala
@@ -4,6 +4,7 @@ import model.cards.special.ChangeColor
 import model.cards.{SimpleCard, SimpleCardImpl, SpecialCard}
 import utils.Color
 import utils.ImageHandler.loadCardImage
+import utils.CardNumber.{Five, Six, Seven}
 
 class HardBotTest extends AnyFunSuite:
 
@@ -13,27 +14,27 @@ class HardBotTest extends AnyFunSuite:
 
   test("The BOT should choose the correct card"):
     val Bot = new HardBotPlayerImpl()
-    Bot.addCard(new SimpleCardImpl(5, Color.Red, loadCardImage("5", Color.Red)))
-    val middleCard = new SimpleCardImpl(5, Color.Blue, loadCardImage("5", Color.Blue))
+    Bot.addCard(new SimpleCardImpl(Five, Color.Red, loadCardImage("5", Color.Red)))
+    val middleCard = new SimpleCardImpl(Five, Color.Blue, loadCardImage("5", Color.Blue))
     val card = Bot.chooseCardToUse(middleCard)
     assert(card.isDefined)
-    Bot.addCard(new SimpleCardImpl(5, Color.Red, loadCardImage("5", Color.Red)))
-    val middleCard2 = new SimpleCardImpl(5, Color.Red, loadCardImage("5", Color.Red))
+    Bot.addCard(new SimpleCardImpl(Five, Color.Red, loadCardImage("5", Color.Red)))
+    val middleCard2 = new SimpleCardImpl(Five, Color.Red, loadCardImage("5", Color.Red))
     val card2 = Bot.chooseCardToUse(middleCard2)
     assert(card.isDefined)
 
   test("The BOT should NOT choose and remove the card, because it's not compatible"):
     val Bot = new HardBotPlayerImpl()
-    Bot.addCard(new SimpleCardImpl(5, Color.Red, loadCardImage("5", Color.Red)))
-    val middleCard = new SimpleCardImpl(6, Color.Blue, loadCardImage("6", Color.Blue))
+    Bot.addCard(new SimpleCardImpl(Five, Color.Red, loadCardImage("5", Color.Red)))
+    val middleCard = new SimpleCardImpl(Six, Color.Blue, loadCardImage("6", Color.Blue))
     val card = Bot.chooseCardToUse(middleCard)
     assert(card.isEmpty)
 
   test("The BOT should choose the Valid Simple Card"):
     val Bot = new HardBotPlayerImpl()
-    Bot.addCard(new SimpleCardImpl(5, Color.Red, loadCardImage("5", Color.Red)))
-    Bot.addCard(new SimpleCardImpl(5, Color.Blue, loadCardImage("6", Color.Blue)))
-    val middleCard = new SimpleCardImpl(7, Color.Red, loadCardImage("7", Color.Red))
+    Bot.addCard(new SimpleCardImpl(Five, Color.Red, loadCardImage("5", Color.Red)))
+    Bot.addCard(new SimpleCardImpl(Five, Color.Blue, loadCardImage("6", Color.Blue)))
+    val middleCard = new SimpleCardImpl(Seven, Color.Red, loadCardImage("7", Color.Red))
     val card = Bot.chooseCardToUse(middleCard)
     assert(card.get.color == Color.Red)
     assert(card.isDefined)
@@ -41,8 +42,8 @@ class HardBotTest extends AnyFunSuite:
   test("The BOT should choose the Valid Special Card"):
     val Bot = new HardBotPlayerImpl()
     Bot.addCard(new ChangeColor(null))
-    Bot.addCard(new SimpleCardImpl(5, Color.Blue, loadCardImage("6", Color.Blue)))
-    val middleCard = new SimpleCardImpl(7, Color.Red, loadCardImage("7", Color.Red))
+    Bot.addCard(new SimpleCardImpl(Five, Color.Blue, loadCardImage("6", Color.Blue)))
+    val middleCard = new SimpleCardImpl(Seven, Color.Red, loadCardImage("7", Color.Red))
     val card = Bot.chooseCardToUse(middleCard)
     assert(card.get.color == Color.Black)
     assert(card.isDefined)
@@ -50,8 +51,8 @@ class HardBotTest extends AnyFunSuite:
   test("The BOT should choose the Special Card"):
     val Bot = new HardBotPlayerImpl()
     Bot.addCard(new ChangeColor(null))
-    Bot.addCard(new SimpleCardImpl(7, Color.Red, loadCardImage("7", Color.Red)))
-    val middleCard = new SimpleCardImpl(7, Color.Red, loadCardImage("7", Color.Red))
+    Bot.addCard(new SimpleCardImpl(Seven, Color.Red, loadCardImage("7", Color.Red)))
+    val middleCard = new SimpleCardImpl(Seven, Color.Red, loadCardImage("7", Color.Red))
     val card = Bot.chooseCardToUse(middleCard)
     assert(card.get.color == Color.Black)
     assert(card.isDefined)
