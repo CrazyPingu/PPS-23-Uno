14e49b0183e12f58ee32c96399cc80033ab9a7ba
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2024-07-31 19:03:27 +0200

    Fix test error

--- src/test/scala/DeckTest.scala
+++ src/test/scala/DeckTest.scala
@@ -1,6 +1,5 @@
 import model.Deck
 import model.cards.SimpleCardImpl
-import model.cards.factory.CardFactoryImpl
 import model.cards.special.{ChangeColor, DrawCard, ReverseCard, SkipCard}
 import org.scalatest.funsuite.AnyFunSuite
 import utils.Color
@@ -11,11 +10,11 @@ import utils.Color
 class DeckTest extends AnyFunSuite:
 
   test("Deck should be initialized with 108 cards"):
-    val deck = new Deck(new CardFactoryImpl())
+    val deck = new Deck()
     assert(deck.size == 108)
 
   test("Deck should contain 19 number cards of each color"):
-    val deck = new Deck(new CardFactoryImpl())
+    val deck = new Deck()
     val colors = Seq(Color.Red, Color.Green, Color.Blue, Color.Yellow)
     for color <- colors do
       val numberCards = deck.collect:
@@ -23,7 +22,7 @@ class DeckTest extends AnyFunSuite:
       assert(numberCards.size == 19)
 
   test("Deck should contain 2 reverse cards of each color"):
-    val deck = new Deck(new CardFactoryImpl())
+    val deck = new Deck()
     val colors = Seq(Color.Red, Color.Green, Color.Blue, Color.Yellow)
     for color <- colors do
       val reverseCards = deck.collect:
@@ -31,7 +30,7 @@ class DeckTest extends AnyFunSuite:
       assert(reverseCards.size == 2)
 
   test("Deck should contain 2 skip cards of each color"):
-    val deck = new Deck(new CardFactoryImpl())
+    val deck = new Deck()
     val colors = Seq(Color.Red, Color.Green, Color.Blue, Color.Yellow)
     for color <- colors do
       val skipCards = deck.collect:
@@ -39,7 +38,7 @@ class DeckTest extends AnyFunSuite:
       assert(skipCards.size == 2)
 
   test("Deck should contain 2 draw 2 cards of each color"):
-    val deck = new Deck(new CardFactoryImpl())
+    val deck = new Deck()
     val colors = Seq(Color.Red, Color.Green, Color.Blue, Color.Yellow)
     for color <- colors do
       val drawCards = deck.collect:
@@ -47,19 +46,19 @@ class DeckTest extends AnyFunSuite:
       assert(drawCards.size == 2)
 
   test("Deck should contain 4 wild draw 4 cards"):
-    val deck = new Deck(new CardFactoryImpl())
+    val deck = new Deck()
     val wildDrawCards = deck.collect:
       case card: DrawCard if card.color == Color.Black && card.numberToDraw == 4 => card
     assert(wildDrawCards.size == 4)
 
   test("Deck should contain 4 change color cards"):
-    val deck = new Deck(new CardFactoryImpl())
+    val deck = new Deck()
     val changeColorCards = deck.collect:
       case card: ChangeColor => card
     assert(changeColorCards.size == 4)
 
   test("Deck should draw a card from the top"):
-    val deck = new Deck(new CardFactoryImpl())
+    val deck = new Deck()
     val firstCard = deck(0)
     val drawnCard = deck.draw()
     assert(drawnCard == firstCard)
