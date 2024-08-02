153b19d1fff107d09cd9f6902a86c47f090e88e0
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2024-08-02 12:56:11 +0200

    Add ScalaDoc

--- src/main/scala/view/tutorial/GeneralTutorialSlide.scala
+++ src/main/scala/view/tutorial/GeneralTutorialSlide.scala
@@ -2,4 +2,11 @@ package view.tutorial
 
 import java.awt.Image
 
+/**
+ * A class representing a general tutorial slide.
+ *
+ * @param image       The image of the slide.
+ * @param title       The title of the slide.
+ * @param description The description of the slide.
+ */
 class GeneralTutorialSlide(val image: Image, val title: String, val description: String)
