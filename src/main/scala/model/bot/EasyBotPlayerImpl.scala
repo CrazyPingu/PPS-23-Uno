64b05199d1b42144a98297347a2f2732ac58b852
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2024-08-02 12:56:20 +0200

    Add ScalaDoc

--- src/main/scala/model/bot/EasyBotPlayerImpl.scala
+++ src/main/scala/model/bot/EasyBotPlayerImpl.scala
@@ -2,6 +2,11 @@ package model.bot
 
 import model.cards.Card
 
+/**
+ * A class representing an easy bot player.
+ * 
+ * An easy bot player plays the first compatible card it finds in its hand.
+ */
 class EasyBotPlayerImpl extends BotPlayerImpl:
   override def chooseCardToUse(centerCard: Card): Option[Card] =
     val compatibleCards = this.filter(
