f4a943772bc7af212bc683db55a5833b39ba7962
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2024-08-02 12:56:05 +0200

    Add ScalaDoc

--- src/main/scala/view/tutorial/TutorialSlideFactory.scala
+++ src/main/scala/view/tutorial/TutorialSlideFactory.scala
@@ -2,8 +2,16 @@ package view.tutorial
 
 import utils.ImageHandler
 
+/**
+ * A factory class for creating tutorial slides.
+ */
 class TutorialSlideFactory:
 
+  /**
+   * Creates a slide that explains how to choose a card.
+   *
+   * @return The slide that explains how to choose a card.
+   */
   def createCardSlide(): GeneralTutorialSlide =
     new GeneralTutorialSlide(
       ImageHandler.chooseCard,
@@ -11,20 +19,35 @@ class TutorialSlideFactory:
       "Click a card in your hand to play it when it's your turn"
     )
 
+  /**
+   * Creates a slide that explains how to draw a card.
+   * 
+   * @return The slide that explains how to draw a card.
+   */
   def createDeckSlide(): GeneralTutorialSlide =
     new GeneralTutorialSlide(
       ImageHandler.drawCard,
       "How to Draw a Card",
       "Draw a card from the deck if you can’t play any of your current cards"
     )
-
+  
+  /**
+   * Creates a slide that explains how to play a card.
+   * 
+   * @return The slide that explains how to play a card.
+   */
   def createCompatibilitySlide(): GeneralTutorialSlide =
     new GeneralTutorialSlide(
       ImageHandler.cardCompatibility,
       "Card Compatibility",
       "Play cards that match the color or number of the top card on the discard pile"
     )
-
+  
+  /**
+   * Creates a slide that explains how to call UNO.
+   * 
+   * @return The slide that explains how to call UNO.
+   */
   def createUnoSlide(): GeneralTutorialSlide =
     new GeneralTutorialSlide(
       ImageHandler.UnoPress,
@@ -32,6 +55,11 @@ class TutorialSlideFactory:
       "Announce \"UNO\" when you have only one card left to avoid a penalty"
     )
 
+  /**
+   * Creates a slide that explains how to win a game.
+   * 
+   * @return The slide that explains how to win a game.
+   */
   def createWinLoseSlide(): GeneralTutorialSlide =
     new GeneralTutorialSlide(
       ImageHandler.tutorialWin,
@@ -39,6 +67,11 @@ class TutorialSlideFactory:
       "Be the first to play all your cards to win the game"
     )
 
+  /**
+   * Creates a slide that explains special cards.
+   * 
+   * @return The slide that explains special cards.
+   */
   def createSpecialCardSlide(): GeneralTutorialSlide =
     new GeneralTutorialSlide(
       ImageHandler.specialCards,
